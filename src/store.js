3abff8e5acd264bc983ced428e1e8f7deb0ae7e7
Joel Watson <joeljameswatson@gmail.com>
2018-09-25 17:09:05 -0700

    format files with prettier

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
+const persistedState = {
+  todos: [{ id: "0", text: "welcome back", completed: false }]
+};
+
 const store = createStore(
   rootReducer,
+  persistedState,
   // for redux devtools: https://github.com/zalmoxisus/redux-devtools-extension#usage
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );

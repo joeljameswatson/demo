870304dd0c5995909fea2a12322146a9e6884aaa
Joel Watson <joeljameswatson@gmail.com>
2018-09-28 12:28:09 -0700

    use middleware

--- src/api/index.js
+++ src/api/index.js
@@ -25,4 +25,16 @@ const fakeDatabase = {
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-export const fetchTodos = filter => delay(500).then(() => fakeDatabase.todos);
+export const fetchTodos = filter =>
+  delay(500).then(() => {
+    switch (filter) {
+      case "all":
+        return fakeDatabase.todos;
+      case "active":
+        return fakeDatabase.todos.filter(t => !t.completed);
+      case "completed":
+        return fakeDatabase.todos.filter(t => t.completed);
+      default:
+        throw new Error(`Unknown filter: ${filter}`);
+    }
+  });
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,26 +1,18 @@
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware, compose } from "redux";
+import promise from "redux-promise";
 import rootReducer from "./reducers";
-
-const addPromiseSupportToStore = store => {
-  const rawDispatch = store.dispatch;
-  return action => {
-    if (typeof action.then === "function") {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
-};
 
 // By exporting configureStore instead of just store, we will be able to create as many store instances as we want for testing.
 const configureStore = () => {
+  // for redux devtools: https://github.com/zalmoxisus/redux-devtools-extension#usage
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middlewares = [promise];
   const store = createStore(
     rootReducer,
-    // for redux devtools: https://github.com/zalmoxisus/redux-devtools-extension#usage
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // persisted state goes here,
+    composeEnhancers(applyMiddleware(...middlewares))
   );
-
-  store.dispatch = addPromiseSupportToStore(store);
 
   return store;
 };

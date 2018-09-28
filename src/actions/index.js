65415c6f412c1ad3cc51fa56c7c0e373ada193a5
Joel Watson <joeljameswatson@gmail.com>
2018-09-28 16:38:41 -0700

    use thunks

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,10 +18,15 @@ const receiveTodos = (filter, response) => ({
   response
 });
 
-export const fetchTodos = filter =>
-  api.fetchTodos(filter).then(response => receiveTodos(filter, response));
+export const fetchTodos = filter => dispatch => {
+  dispatch(requestTodos(filter));
 
-export const requestTodos = filter => ({
+  return api.fetchTodos(filter).then(response => {
+    dispatch(receiveTodos(filter, response));
+  });
+};
+
+const requestTodos = filter => ({
   type: "REQUEST_TODOS",
   filter
 });
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import promise from "redux-promise";
 import rootReducer from "./reducers";
 
 // By exporting configureStore instead of just store, we will be able to create as many store instances as we want for testing.
 const configureStore = () => {
+  const thunk = store => next => action =>
+    typeof action === "function" ? action(store.dispatch) : next(action);
   // for redux devtools: https://github.com/zalmoxisus/redux-devtools-extension#usage
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const middlewares = [promise];
+  const middlewares = [thunk];
   const store = createStore(
     rootReducer,
     // persisted state goes here,

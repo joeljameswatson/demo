3415e530703a364c10a0c6e183db7eaf152c6110
Joel Watson <joeljameswatson@gmail.com>
2018-09-28 16:52:56 -0700

    handle race conditions

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 // By exporting configureStore instead of just store, we will be able to create as many store instances as we want for testing.
 const configureStore = () => {
-  const thunk = store => next => action =>
-    typeof action === "function" ? action(store.dispatch) : next(action);
   // for redux devtools: https://github.com/zalmoxisus/redux-devtools-extension#usage
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
